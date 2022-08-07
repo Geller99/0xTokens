4cbb036fffabfd442e97f8632a92487181ee21aa
geller99 <gellera800@gmail.com>
2022-08-07 10:39:47 -0400

    Fixes

--- contracts/RedToken.sol
+++ contracts/RedToken.sol
@@ -2,14 +2,34 @@
 pragma solidity ^0.8.9;
 
 // Import this file to use console.log
+
+/**
+ * @dev setup access control to deploy Staking Contract with minter and burner roles for reward token
+ * 
+ */
 import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract PawnShop is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Pawn Shop", "PWN") {
-        _mint(msg.sender, initialSupply);
-    
+contract PawnShop is ERC20, AccessControl {
+    // Create a new role identifier for the minter role
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor(address minter, address burner) public ERC20("PawnShop", "PWN") {
+        // Grant the minter role to a specified account
+       _setupRole(MINTER_ROLE, minter);
+       _setupRole(BURNER_ROLE, burner);
     }
-    
+
+    function mint(address to, uint256 amount) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public {
+        require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
+        _burn(from, amount);
+    }
 }
--- contracts/StakingERC20/Staking.sol
+++ contracts/StakingERC20/Staking.sol
@@ -22,21 +22,23 @@ contract Staking {
      */
 
     using Strings for uint256;
-    uint256 private rewardsPercentage = 0.05;
-    uint256 public initialTimestamp;
-    uint256 public timePeriod;
     bool public timestampSet;
     address public owner;
-
+    uint256 private rewardsPercentage = 5;
+    uint256 public initialTimestamp;
+    uint256 public timePeriod;
+    
+ 
      // ERC20 contract address
     IERC20 public erc20TokenContract;
     IERC20 public erc20RewardToken;
 
     // Events for staking, unstaking and claiming rewards
     event TokensStaked(address indexed from, uint256 amount);
-    event TokensUnstaked(address indexed to, uint256 amount, uint256 rewardsAmount);
+    event TokensUnstaked(address indexed to, uint256 amount);
     event RewardClaimed(address indexed claimer, uint256 amount);
 
+    error StakingFailed();
     
     constructor(IERC20 erc20TokenAddress, IERC20 rewardTokenAddress) {
         owner = msg.sender;
@@ -60,13 +62,13 @@ contract Staking {
     mapping (address => Staker) public stakers;
 
     function setTimestamp(uint256 _timePeriodInSeconds) public onlyOwner {
-         require(timestampSet == false, "The time stamp has already been set.");
+        require(timestampSet == false, "The time stamp has already been set.");
         timestampSet = true;
         initialTimestamp = block.timestamp;
-        timePeriod = initialTimestamp + timePeriodInSeconds;
+        timePeriod = initialTimestamp + _timePeriodInSeconds;
     }
 
-    function _getRewards (uint256 _amount) internal view return (uint256) {
+    function _getRewards (uint256 _amount) internal view returns (uint256) {
         return rewardsPercentage * _amount;
     } 
 
@@ -75,41 +77,44 @@ contract Staking {
      * @dev implements staking mechanism for by calling interface methods of the IERC20 standard
      * @dev will calculate rewards according to 'amount' of tokens staked
      */
-    function stakeTokens(IERC20 token, uint256 amount) external public returns (bool) {
+    function stakeTokens(IERC20 token, uint256 amount)  public returns (bool) {
         require(timestampSet == true, "Cannot stake, staking period not yet specified");
-        require(amount <= IERC20(tokenContract).balanceOf(msg.sender), "You do not have enough funds to stake!");
+        require(amount <= IERC20(erc20TokenContract).balanceOf(msg.sender), "You do not have enough funds to stake!");
 
     // if staker already exists, update stake amount, time and recalculate % rewards 
-        if (staker[msg.sender].isStaked == true ) {            
-            token.safeTransferFrom(msg.sender, address(this), amount);
-            staker[msg.sender].amount += amount;
-            staker[msg.sender].stakeTime += 6000;
-            staker[msg.sender].rewards += _getRewards(staker[msg.sender].amount);
+        if (stakers[msg.sender].isStaked == true ) {            
+            token.transferFrom(msg.sender, address(this), amount);
+            stakers[msg.sender].amount += amount;
+            stakers[msg.sender].stakeTime += 6000;
+            stakers[msg.sender].rewards += _getRewards(stakers[msg.sender].amount);
             
         } else {
-
-        token.safeTransferFrom(msg.sender, address(this), amount);
-        Staker newStaker = new Staker(amount, 6000, true, _getRewards(amount));
+        bool success = token.transferFrom(msg.sender, address(this), amount);
+        if (!success) {
+            revert StakingFailed();
+        }
+        Staker memory newStaker = Staker(amount, 6000, _getRewards(amount), true);
         stakers[msg.sender] = newStaker;
         }
 
-        emit tokensStaked(msg.sender, amount);
+        emit TokensStaked(msg.sender, amount);
+        return true;
     } 
 
     function unStakeToken(IERC20 token, uint256 amount) public returns (bool) {
-        require(staker[msg.sender].isStaked == true, "You cannot unstake at this time");
-        require(staker[msg.sender].amount > 0, "You do not have any tokens staked");
+        require(stakers[msg.sender].isStaked == true, "You cannot unstake at this time");
+        require(stakers[msg.sender].amount > 0, "You do not have any tokens staked");
 
         /**
          * @dev autoclaims rewards if staker forgot to claim rewards before unstaking
          * @dev updates staker struct amount and rewards and changes isStaked State
          */
         if (block.timestamp >= timePeriod) {
-            if (staker[msg.sender].rewards > 0) claimRewards();
-            staker[msg.sender].amount -= amount;
-            staker[msg.sender].amount < 1 ? staker[msg.sender].isStaked = false : staker[msg.sender].isStaked = true;
-            staker[msg.sender].amount < 1 ? staker[msg.sender].rewards = 0 : staker[msg.sender].rewards = _getRewards(staker[msg.sender].amount) ;
-            token.safeTransfer(msg.sender, amount);
+            if (stakers[msg.sender].rewards > 0) claimRewards();
+            stakers[msg.sender].amount -= amount;
+            stakers[msg.sender].amount < 1 ? stakers[msg.sender].isStaked = false : stakers[msg.sender].isStaked = true;
+            stakers[msg.sender].amount < 1 ? stakers[msg.sender].rewards = 0 : stakers[msg.sender].rewards = _getRewards(stakers[msg.sender].amount) ;
+            token.transfer(msg.sender, amount);
             emit TokensUnstaked(msg.sender, amount);
         } else {
             revert("Tokens are only available after correct time period has elapsed");
@@ -121,26 +126,26 @@ contract Staking {
      * @dev calls IERC20 mint on the rewards token to the address of the claimer based on their % of rewards
      * @dev Rewards can only be claimed halfway through the staking period
      */
-    function claimRewards() external public returns (bool) {
+    function claimRewards() public returns (bool) {
         require(block.timestamp >= timePeriod/2, "Staking period not yet over, try again later");
-        require(staker[msg.sender].rewards > 0, "You cannot claim rewards at this time");
-        require(staker[msg.sender].isStaked == true, "Cannot claim rewards, not active staker");
+        require(stakers[msg.sender].rewards > 0, "You cannot claim rewards at this time");
+        require(stakers[msg.sender].isStaked == true, "Cannot claim rewards, not active staker");
 
-        IERC20(erc20RewardToken).mint(msg.sender, staker[msg.sender].rewards);
-        staker[msg.sender].rewards = 0;
+        // IERC20(erc20RewardToken).mint(msg.sender, stakers[msg.sender].rewards);
+        stakers[msg.sender].rewards = 0;
         return true;
     }
 
 
-    function getVaulTotalBalance () external public view returns (uint256) {
+    function getVaulTotalBalance () external view returns (uint256) {
       return IERC20(erc20TokenContract).balanceOf(address(this));
     } 
 
-    function getIndividualStakerBalance () external public view returns (uint256) {
+    function getIndividualStakerBalance () public view returns (uint256) {
         return stakers[msg.sender].amount;
     }
 
-    function setRewardsPercentage (uint8 _percentage) public view onlyOwner {
+    function setRewardsPercentage (uint8 _percentage) public onlyOwner {
         rewardsPercentage = _percentage;
     }
 
